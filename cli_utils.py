6150ef0bb8ea27f5095f73c1064fcf9823ebb3c8
Sophie <sophie.zihan.perrot@gmail.com>
2026-02-07 11:47:20 +0000

    created cli_utils.py for interface util functions

---
+++ cli_utils.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def display_question(question):
+    print(question["text"])
+
+def display_results(house):
+    pass
+
+def parse_answers():
+    pass
+
+def get_house():
+    pass
