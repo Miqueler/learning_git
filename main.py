64aa51b73328ede7ae5b940360bae66338f599da
Sophie <sophie.zihan.perrot@gmail.com>
2026-02-07 11:11:56 +0000

    Closes #7 - added welcome text

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3
 
 """Hogwarts house sorter"""
+
+print("Welcome to our Hogwarts house quiz!")
+print("Which house shall you be sorted in? Answer the questions to find out...")
